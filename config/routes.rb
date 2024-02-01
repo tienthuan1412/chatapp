a5a1969a5f75875f07acf8dfe7cea98298983e69
daX-ses-thuan-buitien <tienthuan1412@gmail.com>
2024-02-01 13:59:50 +0700

    init project chat app

---
+++ config/routes.rb
@@ -0,0 +1,21 @@
+Rails.application.routes.draw do
+  get 'rooms/index'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  delete '/signout', to: 'sessions#destroy'
+
+  resources :rooms do
+    resources :messages
+  end
+  resources :users
+  root 'rooms#index'
+end
